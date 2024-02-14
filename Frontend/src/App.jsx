8d6afe9c62cd994dd9531b319295d0f257fbefa0
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-02-14 12:41:57 +0545

    added navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-slate-700'>
-      hello
+    <Router>
+    <div className='bg-gray-300 '>
+      <Navbar />
     </div>
+    </Router>
   )
 }
 
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Routes, Route,Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='w-full text-[16px] p-2 flex justify-around border-b-slate-100 '>
+        <div className='font-bold'>
+         <Link to='/'>Auth App</Link>
+        </div>
+        <div>
+          <ul className='flex gap-4'>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li>
+                <Link to='/sign-in'>Sign in</Link>
+            </li>
+          </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
